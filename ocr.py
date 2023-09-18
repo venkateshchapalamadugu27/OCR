358911c7218e4a8f7df3ed8af571a3c95218f0a5
venkateshchapalamadugu27 <venki9553956591@gmail.com>
2023-09-18 06:42:26 +0530

    first commit

---
+++ ocr.py
@@ -0,0 +1,11 @@
+import cv2 
+import matplotlib.pyplot as Plt 
+from pylab import rcParams 
+import pytesseract 
+rcParams[ 'figure . figsize' ]=8,16 
+filename="week8\download.png" 
+img=cv2. imread(filename) 
+print( 'The Image is : ' )
+Plt.imshow(cv2. cvtColor(img,cv2.COLOR_BGR2RGB) )
+string = pytesseract.image_to_string(img)
+print(string)
